a50fcad2fecd7d501598187009da9d06e7179a0c
mdinicola <the.m.dinicola@gmail.com>
2021-03-29 23:00:30 -0400

    final

---
+++ Deliverable1/Deliverable1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Deliverable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string more = "";
+            do
+            {
+                string outUnit = "";
+                Console.Write("Please input a measurement type: ");
+                string units = Console.ReadLine();
+                Console.Write("Please input an amount: ");
+                double amt = double.Parse(Console.ReadLine());
+                if (units.ToLower().StartsWith("inch"))
+                {
+                    amt *= 3.5;
+                    if(amt!=1)
+                    {
+                        outUnit = "fidget spinners";
+                    }
+                    else
+                    {
+                        outUnit = "fidget spinner";
+                    }
+                }
+                else if (units.ToLower().StartsWith("fidget spinner"))
+                {
+                    amt /= 3.5;
+                    if (amt != 1)
+                    {
+                        outUnit = "inches";
+                    }
+                    else
+                    {
+                        outUnit = "inch";
+                    }
+                }
+                else if (units.ToLower().StartsWith("meme"))
+                {
+                    amt /= 5.0;
+                    if (amt != 1)
+                    {
+                        outUnit = "feet";
+                    }
+                    else
+                    {
+                        outUnit = "foot";
+                    }
+                }
+                else
+                {
+                    amt *= 5.0;
+                    if (amt != 1)
+                    {
+                        outUnit = "memes";
+                    }
+                    else
+                    {
+                        outUnit = "meme";
+                    }
+                }
+                Console.WriteLine(amt.ToString() + " " + outUnit);
+                Console.Write("Do you wish to do another calculation? ");
+                more = Console.ReadLine();
+            } while (String.Equals(more, "yes", StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
